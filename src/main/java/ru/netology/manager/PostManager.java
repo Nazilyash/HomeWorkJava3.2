47074eb3d0a4a8f43a06e474f106dd5c4506ff6d
misharka <fareshta88@gmail.com>
2021-03-24 17:22:06 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(String wallOwnerId, String domain, String query, boolean ownerPostOnly, int count, int offset){
+        return null;
+    }
+
+    public void delete(String wallOwnerId, String postId){
+    }
+}
